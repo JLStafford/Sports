f0ec21c222b17d36716ff87972bcbad2d6beeeb7
STEPHAN CHRISTOPHER HOLLY <stephan@axiomcoding.com>
2017-08-30 11:53:08 -0700

    profile fixed

--- sportsApp/routes/events.js
+++ sportsApp/routes/events.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var knex = require('../db/knex.js');
 
-/* GET ividual indevents page. */
+/* GET individual indevents page. */
 router.get('/:id', function(req, res, next) {
   if(req.cookies.user_id) {
     knex.raw(`SELECT events.*, users.first_name FROM events JOIN atendee_events ON atendee_events.event_id = events.id JOIN users ON users.id = atendee_events.user_id WHERE events.id = ${req.params.id}`)
