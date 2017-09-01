84f4d9127ff24791fcc4ab192c4285006c04cd98
jlstafford <jlowellstafford@gmail.com>
2017-09-01 11:12:36 -0700

    edited production database

--- knexfile.js
+++ knexfile.js
@@ -31,12 +31,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'postgres://cdezepdikwknym:07e0d58c039124d2033018e4e1afa12ebae90b9ce35523a44db520594ddb96b4@ec2-54-225-88-191.compute-1.amazonaws.com:5432/de8t3h56mm1ss6',
-      user: 'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: 'postgres://cdezepdikwknym:07e0d58c039124d2033018e4e1afa12ebae90b9ce35523a44db520594ddb96b4@ec2-54-225-88-191.compute-1.amazonaws.com:5432/de8t3h56mm1ss6',
     pool: {
       min: 2,
       max: 10
